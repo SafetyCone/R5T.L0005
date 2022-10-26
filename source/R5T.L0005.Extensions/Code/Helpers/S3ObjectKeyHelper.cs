efa8750d9cae6ab456892f0c19ed2a7d24909f6e
davidcoats <david@getsafetycone.com>
2022-10-26 04:19:35 -0700

    Interim changes.

--- source/R5T.L0005.Extensions/Code/Helpers/S3ObjectKeyHelper.cs
+++ source/R5T.L0005.Extensions/Code/Helpers/S3ObjectKeyHelper.cs
@@ -7,7 +7,7 @@ namespace Amazon.S3
 {
     public static class S3ObjectKeyHelper
     {
-        public static readonly string DefaultDelimiter = "/";
-        public static readonly string DefaultPrefix = Strings.Null;
+        public const string DefaultDelimiter = "/";
+        public const string DefaultPrefix = Strings.Null;
     }
 }
